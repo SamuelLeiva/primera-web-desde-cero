1b98999e16d714afb0f10855f9a62bbdff6a5740
lpilas24 <leiva20152@gmail.com>
2020-12-26 22:47:08 -0500

    Avance de los forms modal de login y register.

---
+++ dev/js/login-register.js
@@ -0,0 +1,47 @@
+const topbar = document.getElementById('topbar')
+
+const loginModal = document.getElementById('login-modal')
+const registerModal = document.getElementById('register-modal')
+
+const loginForm = document.getElementById('login-form')
+const registerForm = document.getElementById('register-form')
+
+if(topbar){//comprobar si un elemento existe. Si en fututras versiones ya no existe este elemento, evita errores
+    topbar.addEventListener('click', (e) => {
+        if(e.target.parentElement.dataset.type != undefined){//si el elemento tiene un type
+            if(e.target.parentElement.dataset.type == 'login'){//si es de type login
+                loginModal.classList.add('lightbox--show')
+            }else if(e.target.parentElement.dataset.type == 'register'){
+                registerModal.classList.add('lightbox--show')
+            }
+        }
+    })
+}
+
+if(loginModal){
+    loginModal.addEventListener('click', (e) => {
+        if(e.target.classList.contains('lightbox')){
+            loginModal.classList.remove('lightbox--show')
+        }
+    })
+}
+
+if(registerModal){
+    registerModal.addEventListener('click', (e) => {
+        if(e.target.classList.contains('lightbox')){
+            registerModal.classList.remove('lightbox--show')
+        }
+    })
+}
+
+if(loginForm){
+    loginForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+    })
+}
+
+if(registerForm){
+    registerForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+    })
+}
